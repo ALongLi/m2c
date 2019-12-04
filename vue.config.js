321336904aece5c45b2fb4c8debdd96e93b6715c
“lal <lal@xdja.com>
2019-12-04 14:31:42 +0800

    build(electron): add electron-builder
    构建桌面应用

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
  * @Author: lal
  * @Date: 2019-12-03 11:37:46
  * @LastEditors: lal
- * @LastEditTime: 2019-12-03 11:47:49
+ * @LastEditTime: 2019-12-04 09:24:22
  */
 const path = require("path");
 // const environment = require("./environment");
@@ -33,5 +33,29 @@ module.exports = {
   },
   configureWebpack: config => {
     console.log(config);
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        // win: {
+        //   icon: "./public/favicon.ico"
+        // },
+        // mac: {
+        //   icon: "./public/app.png"
+        // }
+        nsis: {
+          oneClick: false, // 是否一键安装
+          allowElevation: true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
+          allowToChangeInstallationDirectory: true, // 允许修改安装目录
+          // installerIcon: "./build/icons/aaa.ico", // 安装图标
+          // uninstallerIcon: "./build/icons/bbb.ico", //卸载图标
+          // installerHeaderIcon: "./build/icons/aaa.ico", // 安装时头部图标
+          createDesktopShortcut: true, // 创建桌面图标
+          createStartMenuShortcut: true, // 创建开始菜单图标
+          shortcutName: "xxxx" // 图标名称
+          // include: "build/script/installer.nsh" // 包含的自定义nsis脚本
+        }
+      }
+    }
   }
 };
